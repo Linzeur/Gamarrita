993512e40cdbef5d341e4419c8a58095024caa5d
Brayan Ciudad <linzeur@gmail.com>
2019-06-04 21:22:50 -0500

    Fix styles and design, Add list products

--- Web for companies/Scripts/ListProducts.js
+++ Web for companies/Scripts/ListProducts.js
@@ -1,18 +1,19 @@
-const URL_SIGN_IN = "/api/v1/products/garments/";
+const URL_SIGN_IN = "api/v1/products/enterprise/garments/";
 var URL_BASE = "";
 
 function requestServer(url, methodHttp, callBackMethod, param) {
   var xhr = new XMLHttpRequest();
   url = URL_BASE + url;
+  if (param == null) url += "?t=" + Math.random();
   auth = sessionStorage.getItem("auth-token").toString();
   xhr.open(methodHttp, url);
-  xhr.setRequestHeader('Authorization', 'Token ' + auth );
-  xhr.onreadystatechange = function () {
-    if  (xhr.readyState == 4) {
+  xhr.setRequestHeader("Authorization", "Token " + auth);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
       if (xhr.status >= 200 && xhr.status < 400) callBackMethod(xhr.response);
       else errorsMessages(xhr.response);
     }
-  }
+  };
   if (param != undefined) xhr.send(param);
   else xhr.send();
 }
@@ -20,26 +21,25 @@ function requestServer(url, methodHttp, callBackMethod, param) {
 window.onload = function() {
   URL_BASE = sessionStorage.getItem("urlBase").toString();
   requestServer(URL_SIGN_IN, "get", listProducts);
-}
+};
 
 function listProducts(result) {
-  var jsonResult = JSON.parse(result);
-  var list = jsonResult["results"], nList = jsonResult["count"];
-  var strTableBody = "";
+  var arrResult = JSON.parse(result);
+  var nList = arrResult.length;
+  var strTableBodyRegulars = "",
+    strTableBodyOffers = "",
+    strTableBody = "";
   var rowList;
   for (var i = 0; i < nList; i++) {
-    rowList = list[i];
-    strTableBody += "<tr>";
+    rowList = arrResult[i];
+    strTableBody = "<tr>";
     strTableBody += "<td>";
-    strTableBody += "<img class=\"img-mini\" src=\"";
+    strTableBody += '<img class="img-mini" src="';
     strTableBody += rowList["photo_1"];
-    strTableBody += "\" alt=\"\">";
+    strTableBody += '" alt="">';
     strTableBody += "</td>";
     strTableBody += "<td>";
     strTableBody += rowList["name"];
-    strTableBody += "</td>";
-    strTableBody += "<td>";
-    strTableBody += rowList["id"];
     strTableBody += "</td>";
     strTableBody += "<td>";
     strTableBody += rowList["precio_size_less"];
@@ -51,24 +51,34 @@ function listProducts(result) {
     strTableBody += rowList["category"]["name"];
     strTableBody += "</td>";
     strTableBody += "<td>";
-    strTableBody += "<img class=\"img-ico\" src=\"images/icons/editar.png\" alt=\"\" onclick=\"editarProducto()\">";
-    strTableBody += "<img class=\"img-ico\" src=\"images/icons/eliminar.png\" alt=\"\" onclick=\"eliminarProducto()\">";
+    strTableBody +=
+      '<img class="img-ico" src="images/icons/editar.png" alt="" onclick="editarProducto()">';
+    strTableBody +=
+      '<img class="img-ico" src="images/icons/eliminar.png" alt="" onclick="eliminarProducto()">';
     strTableBody += "</td>";
     strTableBody += "</tr>";
+    if (rowList["ofert"]) strTableBodyOffers += strTableBody;
+    else strTableBodyRegulars += strTableBody;
   }
-  
-  document.getElementById("tbListProducts").innerHTML = strTableBody;
+
+  document.getElementById(
+    "tbListProductsRegulars"
+  ).innerHTML = strTableBodyRegulars;
+  document.getElementById(
+    "tbListProductsOffers"
+  ).innerHTML = strTableBodyOffers;
 }
 
 function errorsMessages(error) {
+  console.log(error);
   var jsonError = JSON.parse(error);
-  var strError = ""
-  for ( var key in jsonError ) {
-    if ( jsonError.hasOwnProperty(key)) {
+  var strError = "";
+  for (var key in jsonError) {
+    if (jsonError.hasOwnProperty(key)) {
       strError += jsonError[key][0];
       strError += "<br/>";
     }
   }
   document.getElementById("errorMessage").innerHTML = strError;
-  $('#modalError').modal('show');
+  $("#modalError").modal("show");
 }
--- Web for companies/Scripts/SignIn.js
+++ Web for companies/Scripts/SignIn.js
@@ -1,25 +1,25 @@
-const URL_SIGN_IN = "/api/v1/users/login-enterprise/";
+const URL_SIGN_IN = "api/v1/users/login-enterprise/";
 var URL_BASE = "";
 
 function requestServer(url, callBackMethod, param) {
   var xhr = new XMLHttpRequest();
   url = URL_BASE + url;
   xhr.open("post", url);
-  xhr.onreadystatechange = function () {
-    if  (xhr.readyState == 4) {
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
       if (xhr.status >= 200 && xhr.status < 400) callBackMethod(xhr.response);
       else errorsMessages(xhr.response);
     }
-  }
+  };
   if (param != undefined) xhr.send(param);
   else xhr.send();
 }
 
 window.onload = function() {
-  URL_BASE = "http://back.gamarrita.com"
+  URL_BASE = "http://134.209.223.231/";
   sessionStorage.setItem("urlBase", URL_BASE);
-  assignEvents(); 
-}
+  assignEvents();
+};
 
 function assignEvents() {
   document.getElementById("btnSignIn").onclick = function() {
@@ -31,26 +31,30 @@ function assignEvents() {
         form.append("email", txtEmail);
         form.append("password", txtPassword);
         requestServer(URL_SIGN_IN, validationLogin, form);
-      } else document.getElementById("errorPassword").innerHTML = "Este campo es obligatorio";
-    } else document.getElementById("errorEmail").innerHTML = "Este campo es obligatorio";
-  }
+      } else
+        document.getElementById("errorPassword").innerHTML =
+          "Este campo es obligatorio";
+    } else
+      document.getElementById("errorEmail").innerHTML =
+        "Este campo es obligatorio";
+  };
 }
 
 function validationLogin(result) {
-  var jsonMessage = JSON.parse(result)
+  var jsonMessage = JSON.parse(result);
   sessionStorage.setItem("auth-token", jsonMessage["token"]);
   window.location.href = "indexEmpresa.html";
 }
 
 function errorsMessages(error) {
   var jsonError = JSON.parse(error);
-  var strError = ""
-  for ( var key in jsonError ) {
-    if ( jsonError.hasOwnProperty(key)) {
+  var strError = "";
+  for (var key in jsonError) {
+    if (jsonError.hasOwnProperty(key)) {
       strError += jsonError[key][0];
       strError += "<br/>";
     }
   }
   document.getElementById("errorMessage").innerHTML = strError;
-  $('#modalError').modal('show');
+  $("#modalError").modal("show");
 }
